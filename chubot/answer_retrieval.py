02b762cb2f359503fb3165885af0aa01b6866b60
trieuhoangan <trieuhoangan1012@gmail.com>
2019-12-17 17:20:58 +0700

    last chatbot

--- chubot/answer_retrieval.py
+++ chubot/answer_retrieval.py
@@ -1,12 +1,12 @@
 import numpy as np
 import spacy
 import pandas as pd
-
+from gensim.models import KeyedVectors
 #load spacy model
 #Need to install spacy version >2.1
 # and install vi_spacy
 # using: https://github.com/trungtv/vi_spacy
-nlp = spacy.load('vi_spacy_model')
+
 
 class ChitChat:
     def __init__(self, datafile):
@@ -15,46 +15,44 @@ class ChitChat:
         qa = df[['q', 'a']].copy()
         # Remove duplicate questions
         qa.drop_duplicates(subset='q', keep='first', inplace=True)
-        # print(qa.shape)
+
         qa_list = qa[['q', 'a']].values
-        # print(qa_list)
+
         # make vectors as spacy.token.doc.Doc type
         targets = [
-                    nlp(q) for q, a in qa_list
+            q for q, a in qa_list
         ]
-
-        self.targets = targets
-        self.qa_list = qa_list
-    def retrieve_answer(self, question):
+        self.targets=[]
+        self.qa_list=[]
+        word2vecmodel_link = "models/wiki.vi.model.bin"
+        self.word2vec = KeyedVectors.load_word2vec_format(fname=word2vecmodel_link,binary=True,unicode_errors='strict')
+        self.targets.append(targets) 
+        self.qa_list.append(qa_list)
+        # self.nlp = spacy.load('vi_spacy_model')
+    def retrieve_answer(self, question,id_number):
         '''
         Question:: String
         '''
-        idoc = nlp(question.lower())
-        distances = [idoc.similarity(t) for t in self.targets]
-        print(np.argmax(distances))
-        print(np.amax(distances))
-        id=0
-        if np.amax(distances) > 0.8:
-            id = np.argmax(distances)
-        else:
-            if np.amax(distances)<0.6: 
-                id = 0
-            else:
-                id = 1 
-        print(self.qa_list[0])
-        print(self.qa_list[1])
-        #return most similar q & a
-        return self.qa_list[id]
+        distances = [self.word2vec.wmdistance(question.split(" "),t.split(" ")) for t in self.targets[id_number]]
+        print(self.targets[id_number])
+        id=np.argmin(distances)
+        return self.qa_list[id_number][id]
 
     def add_more_data(self,datafile):
-        df = pd.read_csv(datafile, header=None, names=['intent', 'middle', 'q', 'a'])
+        df = pd.read_csv(datafile, header=None, names=['intent', 'q', 'noisyq', 'a'])
         qa = df[['q', 'a']].copy()
+        # Remove duplicate questions
+        qa.drop_duplicates(subset='q', keep='first', inplace=True)
+
         qa_list = qa[['q', 'a']].values
+
+        # make vectors as spacy.token.doc.Doc type
         targets = [
-            nlp(q) for q, a in qa_list
+            q for q, a in qa_list
         ]
-        self.targets.extend(targets)
-        self.qa_list.extend(qa_list)
+        self.targets.append(targets)
+        self.qa_list.append(qa_list)
+
 if __name__ == '__main__':
     chitchat_file = 'data/chitchat.csv'
     chitchat = ChitChat(chitchat_file)
