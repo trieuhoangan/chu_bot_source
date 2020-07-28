a30a36d50404f1f21729cf7a87d51d9d7747eab1
An Mega <trieuhoangan1012@gmail.com>
2020-07-28 14:06:27 +0700

    reset new flow

--- chubot/test_api.py
+++ chubot/test_api.py
@@ -11,30 +11,30 @@ if __name__=="__main__":
     api.load_domain(domain_file)
     api.load_model()
     api.upload_answer()
-    # app = Flask(__name__)
-    # @app.route('/')
-    # def hello_world():
-    #     if request.method == 'GET':
-    #         old_time = datetime.datetime.now()
-    #         mess = request.args.get('mess', '')
-    #         print(mess)
-    #         # line = predict(mess)
-    #         line = api.get_answer(mess)
-    #         # print(str(line))
-    #         new_time = datetime.datetime.now()
-    #         print("chatbot thinking time : ",new_time-old_time)
-    #         return str(line).replace("'",'"')
+    app = Flask(__name__)
+    @app.route('/')
+    def hello_world():
+        if request.method == 'GET':
+            old_time = datetime.datetime.now()
+            mess = request.args.get('mess', '')
+            print(mess)
+            # line = predict(mess)
+            line = api.answer(mess)
+            # print(str(line))
+            new_time = datetime.datetime.now()
+            print("chatbot thinking time : ",new_time-old_time)
+            return str(line).replace("'",'"')
 
-    # #     # return "null"
+    #     # return "null"
 
-    # app.run(host= '0.0.0.0')
-    while True:
-        print("please type your input >>>>>>")
-        text  = input()
-        old_time = datetime.datetime.now()
-        response = api.get_answer(text)
-        new_time = datetime.datetime.now()
-        print("status",api.is_in_session)
-        print("timestamp",api.last_request_moment)
-        print(response)
-        print(new_time-old_time)
+    app.run(host= '0.0.0.0')
+    # while True:
+    #     print("please type your input >>>>>>")
+    #     text  = input()
+    #     old_time = datetime.datetime.now()
+    #     response = api.get_answer(text)
+    #     new_time = datetime.datetime.now()
+    #     print("status",api.is_in_session)
+    #     print("timestamp",api.last_request_moment)
+    #     print(response)
+    #     print(new_time-old_time)
